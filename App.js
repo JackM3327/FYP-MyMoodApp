e6641bbf6841bc9079a6fe2c3f920e7934834b27
Jack McGuire <33735918+JackM3327@users.noreply.github.com>
2020-11-26 18:43:16 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image
+      source={{
+        width: 200,
+        height: 300,
+        uri: "https:/picsum.photos/200/300"
+      }}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
